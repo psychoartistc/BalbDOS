05d6dda4d7f2b1f9f60decdfd1c4590a0cf1a3b9
psycho artist <victoryrobot121@mail.ru>
2026-08-01 22:04:58 +0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "cross compiler environment for nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.pkgsCross.x86_64-embedded.buildPackages.gcc
+          pkgs.gnumake
+          pkgs.qemu
+        ];
+      };
+    };
+}
+
