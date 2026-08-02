40e8e4185044f5976ee2ff94d75dd23b0d7c7130
psycho artist <victoryrobot121@mail.ru>
2026-08-02 23:19:49 +0700

    Document font file

--- kernel/include/font/cp866.h
+++ kernel/include/font/cp866.h
@@ -1,6 +1,10 @@
 #ifndef CP866_H_
 #define CP866_H_
 
+// terminal font
+// taken from this sick collection
+// https://github.com/viler-int10h/vga-text-mode-fonts
+
 extern unsigned char g_CP866_F16[];
 extern unsigned int g_CP866_F16_len;
 
